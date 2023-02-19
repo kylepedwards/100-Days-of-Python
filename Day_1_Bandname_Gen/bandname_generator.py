8d487d17412aeb1151e1205ac4b868e05a5b0e7b
Kyle Edwards <clydesbro@gmail.com>
2023-02-19 16:22:14 -0600

    Project Complete

---
+++ Day_1_Bandname_Gen/bandname_generator.py
@@ -0,0 +1,10 @@
+import os
+
+
+os.system("clear || cls")
+
+print("Welcome to Bandname Generator!")
+user_cityname_input = input("What city did you come from? ")
+user_petname_input = input("What's your pet's name? ")
+
+print(f"\nYour band's name is: {user_cityname_input} {user_petname_input}\n")
