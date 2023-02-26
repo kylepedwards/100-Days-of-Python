d4dd1d087b631f7d6281077825df5195615f040f
Kyle Edwards <clydesbro@gmail.com>
2023-02-26 10:12:08 -0600

    Project Complete

--- Day_8_Caesar_Cipher/README.md
+++ Day_8_Caesar_Cipher/README.md
@@ -1,1 +1,15 @@
 # Day 8: Caesar Cipher
+
+### From [Scientific American](https://www.scientificamerican.com/article/crack-the-code-make-a-caesar-cipher/):
+
+"The Caesar cipher, named after Roman Emperor Julius Caesar is one of the earliest and most widely known ciphers. It is a simple form of a “substitution cipher” where you replace each letter of the alphabet with another letter by shifting the whole alphabet a certain number of letters (wrapping around to the beginning once you reach the end). For example, this would be your key and code if you shift each letter by three spaces:
+
+Plain: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+Cipher: XYZABCDEFGHIJKLMNOPQRSTUVW
+
+So, when you write your message, the letter A gets replaced with X, B gets replaced with Y and so on. For example, the word “HELLO” reads:
+
+Plain: HELLO
+Cipher: EBIIL
+
+In order to decode your message, you need to share the “key” (the number 3) with your friend. After that you can send messages that are written in cipher so other people can't read them!"
--- Day_8_Caesar_Cipher/caesar_cipher.py
+++ Day_8_Caesar_Cipher/caesar_cipher.py
@@ -1,2 +1,41 @@
 import os
 import random
+from art import logo
+
+
+os.system("clear || cls")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for character in start_text:
+        if character in alphabet:
+            position = alphabet.index(character)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += character
+    print(f"\nHere's the {cipher_direction}d result: {end_text}")
+
+
+print(logo)
+
+should_end = False
+
+while not should_end:
+    direction = input("\nEnter 'encode' to encrypt or enter 'decode' to decrypt: ")
+    text = input("Enter your message: ").lower()
+    shift = int(input("Enter the shift number: "))
+
+    shift = shift % 26
+    
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("\nEnter 'yes' to go again or 'no' to exit the program: ")
+    if restart == "no":
+        should_end = True
+        os.system("clear || cls")
+        print("\nGoodbye.\n")
