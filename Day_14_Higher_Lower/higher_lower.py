7f4e5282e728bbb00db91e6c7bc35157bb30418f
Kyle Edwards <clydesbro@gmail.com>
2023-03-04 09:55:35 -0600

    Project Complete

--- Day_14_Higher_Lower/higher_lower.py
+++ Day_14_Higher_Lower/higher_lower.py
@@ -1,2 +1,59 @@
 import os
 import random
+from game_data import data
+
+
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
+
+
+def format_data(account):
+    """Format account into printable format: name, description and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess and returns True 
+        if they got it right or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+def game():
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+        
+        print(f"Compare A: {format_data(account_a)}")
+        print("vs")
+        print(f"Against B: {format_data(account_b)}")
+
+        guess = input("Who has more followers? Enter 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        os.system("clear || cls")
+        if is_correct:
+            score += 1
+            print(f"CORRECT! Current score: {score}\n")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}\n")
+
+
+game()
